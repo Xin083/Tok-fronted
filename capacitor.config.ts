ce145b96d38256dc46733be08e557745340712f7
Xin <373074501@qq.com>
2025-05-21 00:23:43 +0800

    chore: update ios/android build config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,18 @@
-export default {
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
   appId: 'com.leixin.xtok', // 这里填你自己的唯一包名
   appName: 'X-Tok', // 这里填你的应用名称
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    contentInset: 'always',
+    backgroundColor: '#ffffff',
+    preferredContentMode: 'mobile'
+  }
 }
+
+export default config
