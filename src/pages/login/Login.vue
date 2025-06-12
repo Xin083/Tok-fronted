930364495017492dd62558f78b2fee5ef9d3aa6f
Xin <373074501@qq.com>
2025-06-13 03:29:18 +0800

    chore: 校验token

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -90,6 +90,7 @@ import Loading from '../../components/Loading.vue'
 import { onMounted, reactive } from 'vue'
 import { useNav } from '@/utils/hooks/useNav'
 import { _no, _sleep } from '@/utils'
+import { useBaseStore } from '@/store/pinia'
 
 defineOptions({
   name: 'login'
@@ -108,6 +109,13 @@ const data = reactive({
 })
 
 onMounted(() => {
+  const baseStore = useBaseStore()
+  // 检查token，如果存在则跳转到首页
+  const token = window.localStorage.getItem('token') || baseStore.token
+  if (token) {
+    nav('/home')
+    return
+  }
   getPhone()
 })
 
